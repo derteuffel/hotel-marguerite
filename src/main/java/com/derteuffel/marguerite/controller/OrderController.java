0018702d539cdf835f195e5d63161748f5888241
enzo kenza <jeanedouga@gmail.com>
2020-03-07 13:12:00 +0100

    order controller for simple user

--- src/main/java/com/derteuffel/marguerite/controller/OrderController.java
+++ src/main/java/com/derteuffel/marguerite/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +19,7 @@ public class OrderController {
 
     @GetMapping("/order")
     public String findAll(Model model){
+        //model.addAttribute("commandes", commandeRepository.findAllByNumTable(numTable));
         model.addAttribute("commandes", commandeRepository.findAll(Sort.by(Sort.Direction.DESC,"id")));
         return "commandes/all-2";
     }
